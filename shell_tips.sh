3a50f11618963554555a1b3fe6d90418d0744e16
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-04-29 16:13:30 +0100

    Edit a string with sed

--- shell_tips.sh
+++ shell_tips.sh
@@ -3,3 +3,7 @@ echo "$1 $2" | awk '{print ($1+$2-1)/$2}'
 
 # floor division @ awk
 echo "$1 $2" | awk '{print int($1/$2)}'
+
+# edit a string with sed
+sed 's/_/ /g' <<< "el_stringo"
+# >> O/P: "el stringo"
