376e2fa7cb651a96fccc883358083eb42bc015e9
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-05-02 15:47:44 +0100

    HOW TO: sync a fork

--- tips/TipsGITSVN.sh
+++ tips/TipsGITSVN.sh
@@ -26,3 +26,21 @@ git diff --cached '=> diff staging area vs repo'
 find . -iname ".svn" -print0 | xargs -0 rm -r
 #...or...
 find . -name .svn -exec rm -r -f {} +
+
+# SYNCING A FORK
+# c.f. https://help.github.com/articles/syncing-a-fork/
+# 0.0. Configure a remote for a fork
+# 0.1. Check what remotes are configured (if any)
+git remote -v
+# 0.2. Designate a new upstream repo to sync with
+git remote add upstream https://github.com/ORIGINAL_OWNER/ORIGINAL_REPO.git
+# 0.3 Verify new configuration
+git remote -v
+# 1.0 Go to working directory
+? cd /go/to/the/working/dir
+# 1.1 Fetch the original up-to-date repo, save in "upstream/master" branch
+git fetch upstream
+# 1.2 Check out my own fork's local master branch
+git checkout master
+# 1.3 Merge the upstream branch with my local master branch
+git merge upstream/master
