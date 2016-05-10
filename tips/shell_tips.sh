dbbd3494a4ca8846e1838abc89e3ce684cbea180
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-05-10 15:54:02 +0100

    Removed debugging tips (now in dbg.sh), added sed & awk tips

--- tips/shell_tips.sh
+++ tips/shell_tips.sh
@@ -7,3 +7,18 @@ echo "$1 $2" | awk '{print int($1/$2)}'
 # edit a string with sed
 sed 's/_/ /g' <<< "el_stringo"
 # >> O/P: "el stringo"
+
+# replace newline with space
+tr '\n' ' ' < file.ext
+
+# read nth column of file (e.g. 2nd & 3rd cols)
+awk '{ print $2,$3 }' file.ext
+
+# count # lines in file
+wc -l $file
+
+# sed edit file in place; add prefix to beginning of each line
+sed -i -e 's/^/prefix2add/' file
+
+# sed create new file
+sed -i -e 's/^/prefix2add/' oldfile > newfile
