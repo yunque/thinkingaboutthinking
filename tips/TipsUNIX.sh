4ac2dbcff227dcf68e2abc6054b60e33b85dc67b
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-05-24 12:24:16 +0100

    Copy directory tree structure (w/o copying contents)

--- tips/TipsUNIX.sh
+++ tips/TipsUNIX.sh
@@ -66,41 +66,8 @@ Ctrl+R
 len=${#string}
 
 # Read file line-by-line
-while read line; do echo $line; done <file.lst 
+while read line; do echo $line; done <file.lst
 
-# Find the ARM GCC toolchain directory
-locate arm-none-eabi-gcc
-# e.g. @ /home/imr/ti/ccsv6/tools/compiler/gcc-arm-none-eabi-4_8-2014q3/arm-none-eabi/
-
-# Location of assembler (as), linker(ld), etc.
-/usr/arm-linux-gnueabi/bin
-
-
-
-
-# GDB
-# basix @ http://www.thegeekstuff.com/2010/03/debug-c-program-using-gdb/
-# 1. Call a program with $ "gdb /path/to/program.c"
-# 2. Set breakpoints
-# 3. Run program with command line arguments w/ $ "run -m /path/to/model.bsm" etc
-
-# step into
-s
-
-# next
-n
-
-# set breakpoint
-b filename.ext:line#
-
-# delete breakpoint, cf. http://www.delorie.com/gnu/docs/gdb/gdb_32.html
-clear filename.ext:line#
-
-# print variable value
-p VAR
-
-# examine contents of address
-x 0x7FFFFFF
-
-# show current breakpoints
-info breakpoints
+# copy a directory file structure (w/o copying contents)
+find . -type d >dirs.txt
+xargs mkdir -p <dirs.txt
