f6fa868dd61694e2f5ca3fe201cde7403007e525
yunque <i.martinez08@alumni.imperial.ac.uk>
2017-10-26 21:18:54 +0100

    Query plan + planning/execution time

--- tips/sql_tips.sql
+++ tips/sql_tips.sql
@@ -6,3 +6,7 @@ AND    table_name   = 'tablen'
 
 -- Size of table (MB)
 SELECT pg_size_pretty(pg_total_relation_size('"scheman"."tablen"'));
+
+-- Query plan + planning/execution time
+EXPLAIN ANALYZE
+<QUERY>
