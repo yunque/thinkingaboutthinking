cf87b09655c3310fe70bf224de051e469d088649
yunque <i.martinez08@alumni.imperial.ac.uk>
2017-10-26 21:11:43 +0100

    Size of table (MB)

--- tips/sql_tips.sql
+++ tips/sql_tips.sql
@@ -1,5 +1,8 @@
 -- Retrieve a table's colums names
 SELECT column_name
 FROM   information_schema.columns
-WHERE  table_schema = 'schema_name'
-AND    table_name   = 'table_name'
+WHERE  table_schema = 'scheman'
+AND    table_name   = 'tablen'
+
+-- Size of table (MB)
+SELECT pg_size_pretty(pg_total_relation_size('"scheman"."tablen"'));
