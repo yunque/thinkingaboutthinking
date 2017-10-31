af59664d37e6cffdaa14a45d279d016802e25120
yunque <i.martinez08@alumni.imperial.ac.uk>
2017-10-31 17:01:18 +0000

    Size of all materialized views

--- tips/sql_tips.sql
+++ tips/sql_tips.sql
@@ -1,22 +1,43 @@
 -- Retrieve a table's colums names
-SELECT column_name
-FROM   information_schema.columns
-WHERE  table_schema = 'scheman'
-AND    table_name   = 'tablen'
+SELECT
+  column_name
+FROM
+  information_schema.columns
+WHERE
+  table_schema = 'scheman'
+  AND
+  table_name   = 'tablen'
+
 
 -- Size of table (MB)
 SELECT pg_size_pretty(pg_total_relation_size('"scheman"."tablen"'));
 
+
 -- Size of all tables (in a schema, e.g. "dbo")
 SELECT
-    table_schema || '.' || table_name AS table_full_name,
-    pg_size_pretty(pg_total_relation_size('"' || table_schema || '"."' || table_name || '"')) AS SIZE
+  table_schema || '.' || table_name AS table_full_name,
+  pg_size_pretty(pg_total_relation_size('"' || table_schema || '"."' || table_name || '"')) AS SIZE
 FROM
-	information_schema.tables
+  information_schema.tables
 WHERE
-	table_schema = 'dbo'
+  table_schema = 'dbo'
 ORDER BY
-    pg_total_relation_size('"dbo"."' || table_name || '"') DESC
+  pg_total_relation_size('"dbo"."' || table_name || '"') DESC
+
+
+-- Size of all materialized views
+SELECT
+  relname   AS objectname
+, relkind   AS objecttype
+, reltuples AS entries
+, pg_size_pretty(pg_table_size(oid)) AS size  -- depending - see below
+FROM
+  pg_class
+WHERE
+  relkind IN ('m')
+ORDER BY
+  pg_table_size(oid) DESC;
+
 
 -- Query plan + planning/execution time
 EXPLAIN ANALYZE
