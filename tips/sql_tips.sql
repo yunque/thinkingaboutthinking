c24a8e8d5389e6e88f199b8f63eb90399824e868
yunque <i.martinez08@alumni.imperial.ac.uk>
2017-10-31 16:31:34 +0000

    Size of all tables in a schema

--- tips/sql_tips.sql
+++ tips/sql_tips.sql
@@ -7,6 +7,17 @@ AND    table_name   = 'tablen'
 -- Size of table (MB)
 SELECT pg_size_pretty(pg_total_relation_size('"scheman"."tablen"'));
 
+-- Size of all tables (in a schema, e.g. "dbo")
+SELECT
+    table_schema || '.' || table_name AS table_full_name,
+    pg_size_pretty(pg_total_relation_size('"' || table_schema || '"."' || table_name || '"')) AS SIZE
+FROM
+	information_schema.tables
+WHERE
+	table_schema = 'dbo'
+ORDER BY
+    pg_total_relation_size('"dbo"."' || table_name || '"') DESC
+
 -- Query plan + planning/execution time
 EXPLAIN ANALYZE
 <QUERY>
