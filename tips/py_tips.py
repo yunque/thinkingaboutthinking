b270f6e3a1c8708a848d0bddb59c3cf3e5f057c5
yunque <i.martinez08@alumni.imperial.ac.uk>
2017-11-23 09:30:58 +0000

    Misc python tips

---
+++ tips/py_tips.py
@@ -0,0 +1,92 @@
+''' Command Line Arguments '''
+import argparse
+
+parser = argparse.ArgumentParser(description='CLI')
+parser.add_argument('--input', '-i', type='str', required=True)
+args = parser.parse_args()
+print("Input:", args.input)
+
+
+''' Read a gzipped csv file '''
+import gzip
+import csv
+
+with gzip.open('file.csv.gz', 'rb') as fp:
+  reader = csv.reader(fp)
+  for row in reader:
+    print(row)
+
+
+''' Vector multiplication (dot product) '''
+import numpy as np
+
+w = np.array([2,2,2,2])
+x = np.array([1,2,3,4])
+y = np.dot(w,x)
+
+
+''' Generator MxN random numbers '''
+import numpy as np
+
+np.random.random((M,N))
+
+
+''' Time a function '''
+import time
+
+tic = time.time()
+# A function!
+toc = time.time()
+print(toc - tic)
+
+
+''' Remove list from list '''
+new_list = [i for i in old_list if i not in list_of_items_to_remove]
+
+
+''' Read csv header only '''
+import csv
+
+with open(fpath, 'r') as fp:
+    reader = csv.DictReader(fp)
+    fieldnames = reader.fieldnames
+
+''' List files in directory '''
+import os
+print os.listdir(<directory_path>)
+
+
+''' Initialize empty dictionary of lists '''
+keys = ['k1', 'k2', 'k3']
+d = dict.fromkeys(keys, [])
+
+
+''' Extract column values/index from a DataFrame '''
+df["col"].values
+df["col"].index
+
+
+''' Concatenate two DataFrames along the same index '''
+df_both = pd.concat([df_one, df_two], axis=1)
+
+
+''' Sum all values in a df column that have value x '''
+sum_cols_equal_to_x = df[df['col'] == x].sum()['col']
+
+
+''' Slice a df with a Series to extract a the rows which match a list of values '''
+series = pd.Series([list, of, items, to, view]
+df[df['col'].isin(series)]
+
+
+''' Unique elements'''
+# array
+np.unique(array)
+# list
+set(lst)
+# DataFrame column
+df['col'].unique()
+# DataFrame (distinct rows)
+df.drop_duplicates()
+# DataFrames (distinct entries in a specific column)
+df.drop_duplicates(subset='col', keep='first')
