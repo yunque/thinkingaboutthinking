839fb0f24e835ee8f54c608d46524174af2b07c9
yunque <i.martinez08@alumni.imperial.ac.uk>
2018-03-01 11:29:45 +0000

    vim tips! undo/redo, cut/paste, search/replace

---
+++ tips/vim_tips.sh
@@ -0,0 +1,28 @@
+# Undo/redo
+u
+Ctrl+r
+
+# CUT/PASTE
+# yank (copy) a line
+yy
+# yank to end of line
+y$
+# cut "
+d$
+# cut 2 lines
+2dd
+# put (paste) after/before cursor
+p
+P
+
+# SEARCH/REPLACE
+# forward/backward search
+/pattern
+?pattern
+# forward/backward search continue (next)
+n
+N
+# replace all <old> with <new> in file
+:%s/<old>/<new>/g
+# replace all <old> with <new> in file, with confirmation
+:%s/<old>/<new>/gc
