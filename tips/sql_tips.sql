d2726e5e7d5204658a3ab64b13e45c6e9cdc2321
yunque <yunque@users.noreply.github.com>
2019-07-10 17:12:19 +0100

    Terminate idle processes

--- tips/sql_tips.sql
+++ tips/sql_tips.sql
@@ -111,3 +111,39 @@ SHOW ALL;
 
 -- Find table that contains a given column
 SELECT schema_name, table_name FROM all_tab_columns WHERE column_name = 'ID';
+
+					    
+-- Terminate idle processes older than 2 days
+WITH inactive_connections AS
+(
+	SELECT
+		current_timestamp - state_change AS time_elapsed,
+		state,
+        pid,
+        rank() over (partition by client_addr order by backend_start ASC) as RANK,
+        query
+    FROM 
+        pg_stat_activity AS procs
+    WHERE
+        -- Exclude the thread owned connection (ie no auto-kill)
+        pid <> pg_backend_pid( )
+    AND
+        -- Exclude known applications connections
+        application_name !~ '(?:psql)|(?:pgAdmin.+)'
+    AND
+        -- Include connections to the same database the thread is connected to
+        datname = current_database() 
+    AND
+        -- Include connections using the same thread username connection
+        usename = current_user 
+    AND
+        -- Include inactive connections only
+        state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled') 
+    AND
+        -- Include old connections (found with the state_change field)
+        current_timestamp - state_change > interval '2 days'
+)
+SELECT
+    pg_terminate_backend(pid)
+FROM
+    inactive_connections;
